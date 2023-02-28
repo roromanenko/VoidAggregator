b1d629e4a56a65543ec917e3e471d34c2d847a90
Yevhenii Romanenko <eromanenko@dmvdesk.com>
2023-02-28 13:36:15 +0200

    add file blob storage

---
+++ VoidAggregator.Bl/Settings/StorageSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidAggregator.Bl.Settings
+{
+	public class StorageSetting
+	{
+		public string Path { get; set; }
+	}
+}
