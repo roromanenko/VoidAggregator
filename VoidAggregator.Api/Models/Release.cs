1fea2ae472150c6abb041bd66b90422f6ad85866
Yevhenii Romanenko <eromanenko@dmvdesk.com>
2023-02-28 15:16:18 +0200

    add author model to release

--- VoidAggregator.Api/Models/Release.cs
+++ VoidAggregator.Api/Models/Release.cs
@@ -6,8 +6,8 @@ namespace VoidAggregator.Api.Models
     {
         public int Id { get; set; }
         public string ReleaseName { get; set; }
-        public int AuthorId { get; set; }
-        public Genre Genre { get; set; }
+		public Author Author { get; set; }
+		public Genre Genre { get; set; }
         public List<Song> Songs { get; set; } = new List<Song>();
         public Language Language { get; set; }
         public Label Label { get; set; }
